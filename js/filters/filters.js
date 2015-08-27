02d7bc3e8d76df74dad845a3c5350a34b1828c9b
Hayat Noor <hnoor@bbg.gov>
2015-08-27 16:26:18 -0400

    show full network names

--- js/filters/filters.js
+++ js/filters/filters.js
@@ -28,5 +28,51 @@ angular.module('RDash').filter('reverse', function() {
 	};
 });
 
+// show full network names
+angular.module('RDash').filter('showFullNetworkNames', function() {
+	return function(networks) {
+		for (var i = 0; i < networks.length; i++) {
+			switch(networks[i].name) {
+				case 'HB':
+					networks[i].fullName = 'Current Time (HB)';
+					break;
+				case 'MBN':
+					networks[i].fullName = 'Middle East Broadcasting Network (MBN)';
+					break;
+				case 'OCB':
+					networks[i].fullName = 'Office of Cuba Broadcasting (OCB)';
+					break;
+				case 'RFA':
+					networks[i].fullName = 'Radio Free Asia (RFA)';
+					break;
+				case 'RFE/RL':
+					networks[i].fullName = 'Radio Free Europe / Radio Liberty (RFE/RL)';
+					break;
+				case 'VOA':
+					networks[i].fullName = 'Voice of America (VOA)';
+					break;
+				case 'VOA Central':
+					networks[i].fullName = 'VOA Central';
+					break;
+				default:
+					networks[i].fullName = '';
+			}
+		}
+		return networks;
+
+		/*
+		 Current Time (HB)
+		 Middle East Broadcasting Network (MBN)
+		 Office of Cuba Broadcasting (OCB)
+		 Radio Free Asia (RFA)
+		 Radio Free Europe / Radio Liberty (RFE/RL)
+		 Voice of America (VOA)
+		 VOA Central
+		 */
+	};
+});
+
+
+
 
 
